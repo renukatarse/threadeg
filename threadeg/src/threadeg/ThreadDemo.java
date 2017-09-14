775f42d219e6f25b87cf58ee66576c883ae071f3
renuka <renutarse1@gmail.com>
2017-09-14 17:27:03 +0530

    threadeg commit

---
+++ threadeg/src/threadeg/ThreadDemo.java
@@ -0,0 +1,13 @@
+package threadeg;
+
+public class ThreadDemo {
+
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		
+		Object o = Class.forName(args[0]).newInstance();
+		}
+
+	
+
+}
